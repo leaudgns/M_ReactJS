f3374ddedda4abe4058621e72c8fc597c19b1552
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-04-13 16:44:18 +0900

    Revert "M_ReactJS 3.3"
    This reverts commit a4142ee2e104289db390e4904c7a1587105b16bf.

--- nomflix/src/Components/App.js
+++ nomflix/src/Components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import Router from "./Router";
+import Header from "./Header/Header";
 
 class App extends Component {
   render() {
     return (
       <>
+        <Header />
         <Router />
       </>
     );
