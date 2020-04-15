828907bcf7a00bfb9caeeb43c4c6abf453ae2e88
leaudgns <62051982+leaudgns@users.noreply.github.com>
2020-04-15 21:42:03 +0900

    M_ReactJS 5.3

--- nomflix/src/Routes/TV/TVContainer.js
+++ nomflix/src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { tvApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -10,8 +11,30 @@ export default class extends React.Component {
     error: null,
   };
 
+  async componentDidMount() {
+    try {
+      const {
+        data: { result: topRated },
+      } = await tvApi.topRated();
+      const {
+        data: { result: popular },
+      } = await tvApi.popular();
+      const {
+        data: { result: airingToday },
+      } = await tvApi.airingToday();
+      this.setState({ topRated, popular, airingToday });
+    } catch {
+      this.setState({
+        error: "Can't find TV information.",
+      });
+    } finally {
+      this.setstate({ loading: false });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
+    console.log(this.state);
     return (
       <TVPresenter
         topRated={topRated}
